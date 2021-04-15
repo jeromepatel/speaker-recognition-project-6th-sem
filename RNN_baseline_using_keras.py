9c9e9943526359834bec231efaf8f9c31dd8980d
Jyot Makadiya <57573673+jeromepatel@users.noreply.github.com>
2021-04-15 22:31:23 +0530

    initial baseline file added

---
+++ RNN_baseline_using_keras.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 15 21:52:44 2021
+
+@author: jyotm
+"""
+
+import os
+import shutil
+import numpy as np
+
+import tensorflow as tf
+from tensorflow import keras
+
+from pathlib import Path
+from IPython.display import display, Audio
+
+# Get the data from https://www.kaggle.com/kongaevans/speaker-recognition-dataset/download
+# and save it to the 'Downloads' folder in your HOME directory
+DATASET_ROOT = os.path.join(os.getcwd(), "16000_pcm_speeches/")
+
+# print("*"*12, os.getcwd(), DATASET_ROOT)
+# The folders in which we will put the audio samples and the noise samples
+AUDIO_SUBFOLDER = "audio"
+NOISE_SUBFOLDER = "noise"
+
+DATASET_AUDIO_PATH = os.path.join(DATASET_ROOT, AUDIO_SUBFOLDER)
+DATASET_NOISE_PATH = os.path.join(DATASET_ROOT, NOISE_SUBFOLDER)
+
+# Percentage of samples to use for validation
+VALID_SPLIT = 0.1
+
+# Seed to use when shuffling the dataset and the noise
+SHUFFLE_SEED = 43
+
+# The sampling rate to use.
+# This is the one used in all of the audio samples.
+# We will resample all of the noise to this sampling rate.
+# This will also be the output size of the audio wave samples
+# (since all samples are of 1 second long)
+SAMPLING_RATE = 16000
+
+# The factor to multiply the noise with according to:
+#   noisy_sample = sample + noise * prop * scale
+#      where prop = sample_amplitude / noise_amplitude
+SCALE = 0.5
+
+BATCH_SIZE = 128
+EPOCHS = 100
+
+# If folder `audio`, does not exist, create it, otherwise do nothing
+os.makedirs(DATASET_AUDIO_PATH, exist_ok=True)
+
+# If folder `noise`, does not exist, create it, otherwise do nothing
+os.makedirs(DATASET_NOISE_PATH, exist_ok=True)
+
+for folder in os.listdir(DATASET_ROOT):
+    if os.path.isdir(os.path.join(DATASET_ROOT, folder)):
+        if folder in [AUDIO_SUBFOLDER, NOISE_SUBFOLDER]:
+            # If folder is `audio` or `noise`, do nothing
+            continue
+        elif folder in ["other", "_background_noise_"]:
+            # If folder is one of the folders that contains noise samples,
+            # move it to the `noise` folder
+            shutil.move(
+                os.path.join(DATASET_ROOT, folder),
+                os.path.join(DATASET_NOISE_PATH, folder),
+            )
+        else:
+            # Otherwise, it should be a speaker folder, then move it to
+            # `audio` folder
+            shutil.move(
+                os.path.join(DATASET_ROOT, folder),
+                os.path.join(DATASET_AUDIO_PATH, folder),
+            )
+
+
+# Get the list of all noise files
+noise_paths = []
+for subdir in os.listdir(DATASET_NOISE_PATH):
+    subdir_path = Path(DATASET_NOISE_PATH) / subdir
+    if os.path.isdir(subdir_path):
+        noise_paths += [
+            os.path.join(subdir_path, filepath)
+            for filepath in os.listdir(subdir_path)
+            if filepath.endswith(".wav")
+        ]
+
+print(
+    "Found {} files belonging to {} directories".format(
+        len(noise_paths), len(os.listdir(DATASET_NOISE_PATH))
+    )
+)
+
+command = (
+    "for dir in `ls -1 " + DATASET_NOISE_PATH + "`; do "
+    "for file in `ls -1 " + DATASET_NOISE_PATH + "/$dir/*.wav`; do "
+    "sample_rate=`ffprobe -hide_banner -loglevel panic -show_streams "
+    "$file | grep sample_rate | cut -f2 -d=`; "
+    "if [ $sample_rate -ne 16000 ]; then "
+    "ffmpeg -hide_banner -loglevel panic -y "
+    "-i $file -ar 16000 temp.wav; "
+    "mv temp.wav $file; "
+    "fi; done; done"
+)
+
+os.system(command)
+
+# Split noise into chunks of 16000 each
+def load_noise_sample(path):
+    sample, sampling_rate = tf.audio.decode_wav(
+        tf.io.read_file(path), desired_channels=1
+    )
+    if sampling_rate == SAMPLING_RATE:
+        # Number of slices of 16000 each that can be generated from the noise sample
+        slices = int(sample.shape[0] / SAMPLING_RATE)
+        sample = tf.split(sample[: slices * SAMPLING_RATE], slices)
+        return sample
+    else:
+        print("Sampling rate for {} is incorrect. Ignoring it".format(path))
+        return None
+
+
+noises = []
+for path in noise_paths:
+    sample = load_noise_sample(path)
+    if sample:
+        noises.extend(sample)
+noises = tf.stack(noises)
+
+print(
+    "{} noise files were split into {} noise samples where each is {} sec. long".format(
+        len(noise_paths), noises.shape[0], noises.shape[1] // SAMPLING_RATE
+    )
+)
